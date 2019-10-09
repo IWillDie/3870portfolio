daf427a827c569e9cfc6e113f695e459533fdf5e
Peter Chang <22145568+PCPetC@users.noreply.github.com>
2019-10-08 18:27:33 -0700

    edible_bugs_upload

---
+++ edible_insect_finder/edible_bugs.js
@@ -0,0 +1,29 @@
+/*
+Peter Chang
+Project 2
+CSUEB FA2019
+MM621: Screen Based Interaction
+Instructor: Gwyan Rhabyt
+10-8-2019
+*/
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  background(random(255));
+  fill(random(255),random(255),random(255));  
+
+}
+
+function draw(){
+
+    rect(50,50,windowWidth/2,windowHeight/2);
+
+}
+
+function mouseClicked() {
+
+}
+
+function keyPressed() {
+
+}
